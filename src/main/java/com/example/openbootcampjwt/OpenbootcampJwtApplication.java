c23ef57545d12833544bd40692c66374c8d50153
Edwin <edwintorres0115@hotmail.com>
2024-02-29 09:46:01 -0500

    configuración del pm.xml

---
+++ src/main/java/com/example/openbootcampjwt/OpenbootcampJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.openbootcampjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbootcampJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenbootcampJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/openbootcampjwt/OpenbootcampJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openbootcampjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbootcampJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
